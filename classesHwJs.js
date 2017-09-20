3b9d93714645078e59f5c1ca874708d8266aa2dc
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-09-20 16:54:50 -0500

    Commit 4 - Created Person Class

--- classesHwJs.js
+++ classesHwJs.js
@@ -28,7 +28,7 @@ class Dog extends Pet {
 	}
 };
 
-const sparky = new Dog('Sparky', 100);
+const sparky = new Dog('Sparky', 3);
 sparky.setOwner('Robert');
 sparky.bark();
 sparky.chaseTail();
@@ -51,11 +51,74 @@ class Cat extends Pet{
 	}
 };
 
-const sprinkles = new Cat('Sprinkles', 200);
+const sprinkles = new Cat('Sprinkles', 5);
 sprinkles.setOwner('Karen');
 sprinkles.purr();
 sprinkles.clean();
 sprinkles.getPrice();
 console.log(sprinkles);
+
+
+class Person{
+	constructor(name){
+		this.name = name;
+		this.age = 0;
+		this.weight = 0;
+		this.mood = 0;
+		this.pets = [];
+		this.bankAccount = 0;
+	}
+	getName(){
+		return this.name;
+	}
+	getAge(){
+		return this.age;
+	}
+	getWeight(){
+		return this.weight;
+	}
+	greet(other_person){
+		console.log("Hello " + other_person);
+	}
+	eat(){
+		this.weight += 30;
+		this.mood += 10;
+	}
+	excercise(){
+		this.weight -= 10;
+	}
+	newAge(){
+		this.age += 25;
+		this.weight +=75;
+		this.mood -= 5;
+		this.bankAccount += 10;
+	}
+	buyPet(pet){
+		this.pets.push(pet);
+		this.mood += 10;
+		pet.getPrice();
+		this.bankAccount = this.bankAccount - pet.getPrice();
+	}
+}
+
+const jill = new Person('Jill');
+jill.getName();
+jill.getAge();
+jill.getWeight();
+jill.greet('Dylan');
+jill.eat();
+jill.excercise();
+jill.newAge();
+jill.buyPet(sprinkles);
+sprinkles.setOwner('Jill');
+console.log(jill);
+
+
+
+
+
+
+
+
 
 
